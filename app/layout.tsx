bd8442b1f8fd323b18dd67899b7911d3813a2390
dogunfx <dogunfx@gmail.com>
2025-11-27 20:58:12 +0100

    updated meta desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from 'sonner';
 import Script from 'next/script';
-import { LIVE_CHAT } from '@/global/constants';
+import { LIVE_CHAT, SITE_NAME } from '@/global/constants';
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -17,8 +17,8 @@ import { LIVE_CHAT } from '@/global/constants';
 // });
 
 export const metadata: Metadata = {
-  title: '',
-  description: 'Trading at its best',
+  title: `${SITE_NAME} `,
+  description: `Licensed and Regulated Forex and Cryptocurrency Broker, Powerful platforms for trading both on real and demo accounts`,
 };
 
 export default function RootLayout({
