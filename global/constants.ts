b2bdb520e9862ee324ab9077dbb4c52571a81874
dogunfx <dogunfx@gmail.com>
2025-11-28 19:51:34 +0100

    added livechat

--- global/constants.ts
+++ global/constants.ts
@@ -5,4 +5,12 @@ export const PROFILE_IMAGE_PLACE_HOLDER: string = '/images/profile.jpg';
 export const SITE_URL_WITH_HTTPS: string = `https://${SITE_URL}`;
 export const LIVE_CHAT = `
 
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '901de93f38f0e4f2ea0d0994ebee3010c2229996';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
   `;
